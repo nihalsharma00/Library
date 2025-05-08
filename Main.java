7a519e56ff98d6582e04b32ff68cc480d9f6fbf0
nihalsharma00 <nihalsharma3007@gmail.com>
2025-05-08 23:31:06 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        BookManager manager = new BookManager();
+        try {
+            manager.addBook("Java Basics", "John Smith", "Pearson", 2021, "978-1234567890", 5);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
 
