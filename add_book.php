61a41b248c1d787db2dde7dd89a9e69be9dca3b9
nihalsharma00 <nihalsharma3007@gmail.com>
2025-05-08 23:32:39 +0530

    Update add_book.php

--- add_book.php
+++ add_book.php
@@ -1,1 +1,25 @@
+<?php
+$conn = new mysqli("localhost", "root", "", "library");
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $title = $_POST['title'];
+    $author = $_POST['author'];
+    $publisher = $_POST['publisher'];
+    $year = $_POST['year'];
+    $isbn = $_POST['isbn'];
+    $qty = $_POST['quantity'];
+
+    $conn->query("INSERT INTO books (title, author, publisher, year, isbn, quantity)
+                  VALUES ('$title', '$author', '$publisher', $year, '$isbn', $qty)");
+    echo "Book added.";
+}
+?>
+<form method="POST">
+    <input name="title" placeholder="Title" required>
+    <input name="author" placeholder="Author" required>
+    <input name="publisher" placeholder="Publisher">
+    <input name="year" type="number" placeholder="Year">
+    <input name="isbn" placeholder="ISBN">
+    <input name="quantity" type="number" placeholder="Quantity" required>
+    <button type="submit">Add Book</button>
+</form>
