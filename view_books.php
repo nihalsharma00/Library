e6138daef3fc7b42baf947e5fafc880290a97bf7
nihalsharma00 <nihalsharma3007@gmail.com>
2025-05-08 23:32:13 +0530

    Update view_books.php

--- view_books.php
+++ view_books.php
@@ -1,1 +1,10 @@
+<?php
+$conn = new mysqli("localhost", "root", "", "library");
+$result = $conn->query("SELECT * FROM books");
+
+echo "<h2>Books</h2><table border='1'><tr><th>ID</th><th>Title</th><th>Author</th><th>Qty</th></tr>";
+while ($row = $result->fetch_assoc()) {
+    echo "<tr><td>{$row['id']}</td><td>{$row['title']}</td><td>{$row['author']}</td><td>{$row['quantity']}</td></tr>";
+}
+echo "</table>";
 
