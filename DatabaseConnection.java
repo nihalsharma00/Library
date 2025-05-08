789a940470c544877b9d71711aa6d059ee0fdc7d
nihalsharma00 <nihalsharma3007@gmail.com>
2025-05-08 23:31:38 +0530

    Update DatabaseConnection.java

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -1,1 +1,8 @@
+import java.sql.*;
+
+public class DatabaseConnection {
+    public static Connection connect() throws SQLException {
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/library", "root", "");
+    }
+}
 
